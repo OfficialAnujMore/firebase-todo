cf689eb0cff393aa9f437210df8f841fc3aaa488
Anuj C More <moreanuj1307@gmail.com>
2021-12-18 13:14:55 +0530

    Logic developed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
-
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [todo, setTodo] = useState([
+    "Develop react todo list",
+    "Prepare for exam",
+    "Pay bike maintenance",
+  ]);
+  const [input, setInput] = useState("");
+
+  console.log(input);
+
+  const addTodo = (event) => {
+    event.preventDefault();
+    setTodo([...todo, input]);
+    setInput("");
+  };
+
   return (
     <div className="App">
-      <h1>Hello Anuj</h1>
-     
+      <form action="">
+        <input
+          placeholder="Enter your task"
+          value={input}
+          onChange={(event) => setInput(event.target.value)}
+        />
+
+        <button onClick={addTodo}>Add todo</button>
+
+        <ul>
+          {todo.map((item) => (
+            <li>{item}</li>
+          ))}
+        </ul>
+      </form>
     </div>
   );
 }
